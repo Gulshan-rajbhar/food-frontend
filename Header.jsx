c182aaa66a1fab5780bea7745102c484ee705542
Gulshan-rajbhar <grajbhar914@gmail.com>
2024-09-28 10:03:20 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className="header-contents">
+                <h2>Order is the your favraties food header</h2>
+               
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, corrupti delectus! Facilis neque blanditiis omnis ullam atque. Id aperiam non, omnis laboriosam nihil repellendus. Assumenda odio architecto ad asperiores ex?</p>
+            <button>View menu</button>
+           
+            </div>
+
+        </div>
+    )
+}
+
+export default Header
